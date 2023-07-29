e2f792e4aba6f3972d651fa42e36d173fc21fd33
Long Phu <longphu257@gmail.com>
2023-07-29 11:33:13 +0700

    Update Contract

--- src/MyContract.sol
+++ src/MyContract.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@thirdweb-dev/contracts/base/ERC20Base.sol";
+import "@thirdweb-dev/contracts/base/ERC20DropVote.sol";
 
-contract MyContract is ERC20Base {
-    constructor(string memory _name, string memory _symbol) ERC20Base(_name, _symbol) {}
+contract MyContract is ERC20DropVote {
+    constructor(address _defaultAdmin, string memory _name, string memory _symbol, address _primarySaleRecipient)
+        ERC20DropVote(_defaultAdmin, _name, _symbol, _primarySaleRecipient)
+    {}
 }
